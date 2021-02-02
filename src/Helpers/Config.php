58f06599f792f0449a33d47a0ac3aad92c5d0d95
AliRazavi <alirazavi253@gmail.com>
2021-02-03 00:58:29 +0330

    add helper function get file content with key

---
+++ src/Helpers/Config.php
@@ -0,0 +1,28 @@
+<?php 
+namespace App\Helpers;
+
+use App\Exceptions\ConfigfileNotFoundExceptions;
+
+class Config{
+    public static function getFileContent(string $fileName){
+        $filePath=realpath(__DIR__."/../Configs/{$fileName}");
+
+        if (!$filePath){
+            throw new ConfigfileNotFoundExceptions();
+        }
+
+        $fileContent=require_once $filePath;
+
+        return $fileContent;
+    }
+
+    public static function get(string $fileName,string $key){
+        $fileContent=self::getFileContent($fileName);
+
+        if (is_null($key)){
+            return $fileContent;
+        }
+
+        return $fileContent[$key] ?? null;
+    }
+}
