840ca5ecae28e351f9c1a4d8fc087f82c791110b
AliRazavi <alirazavi253@gmail.com>
2021-02-03 08:38:53 +0330

    add pdo database connection

---
+++ src/Database/PDODatabseConnection.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App\Database;
+
+use App\Contracts\DatabaseConnectionInterface;
+
+class PDODatabseConnection implements DatabaseConnectionInterface{
+    public function connect(){
+        
+    }
+
+    public function getConnection(){
+        
+    }
+}
