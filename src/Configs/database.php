19206f9c772369cfebe097ac8d73b673b2258b53
AliRazavi <alirazavi253@gmail.com>
2021-02-03 08:38:17 +0330

    add databse testing settings

--- src/Configs/database.php
+++ src/Configs/database.php
@@ -7,5 +7,13 @@ return [
         'db_user'=>'root',
         'db_name'=>'orm',
         'db_password'=>'123456789',
+    ],
+
+    "pdo_test"=>[
+        'driver'=>'mysql',
+        'db_host'=>'127.0.0.1',
+        'db_user'=>'root',
+        'db_name'=>'orm_testing',
+        'db_password'=>'123456789',
     ]
 ];
