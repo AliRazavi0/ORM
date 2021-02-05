a06e5b5a64e52e5dd6be73f5089f1a88afb949af
AliRazavi <razavi.ali.1998@gmail.com>
2021-02-05 23:35:36 +0330

    add query update

--- src/Database/PDOQueryBuilder.php
+++ src/Database/PDOQueryBuilder.php
@@ -6,6 +6,8 @@ use App\Contracts\DatabaseConnectionInterface;
 class PDOQueryBuilder{
     protected $table;
     protected $connection;
+    protected $conditions;
+    protected $values;
 
     public function __construct(DatabaseConnectionInterface $database)
     {   
@@ -35,6 +37,32 @@ class PDOQueryBuilder{
         $query->execute(array_values($data));
 
         return (int)$this->connection->lastInsertId();
+    }
+
+    public function where(string $column,string $value ){
+
+        $this->conditions[]="{$column}=?";
+
+        $this->values[]=$value;
+
+
+        return $this;
+    }
+
+    public function update(array $data){
+        $fields=[];
+        foreach ($data as $column => $value) {
+            $fields[]="{$column}='{$value}'";
+        }
+        $fields=implode(',',$fields);
+        $conditions=implode(' and ',$this->conditions);
+        $sql="UPDATE {$this->table} SET {$fields} WHERE {$conditions}";
+
+        $query=$this->connection->prepare($sql);
+        $query->execute($this->values);
+
+        return $query->rowCount();
+
     }
 
 
